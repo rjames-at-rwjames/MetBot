68d84ae76b77bba24a8621b24438b7db6a63529e
rjames-at-rwjames <rachel.james@eci.ox.ac.uk>
2019-04-29 11:29:00 +0100

    Edits to subsampling CBs

--- coupanal/paperfigs/CBvar_4panelfig.py
+++ coupanal/paperfigs/CBvar_4panelfig.py
@@ -11,35 +11,40 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 
 cwd=os.getcwd()
 sys.path.append(cwd)
 sys.path.append(cwd+'/..')
 sys.path.append(cwd+'/../..')
 import MetBot.dset_dict as dsetdict
-import MetBot.mast_dset_dict as mast_dict
-import MetBot.dimensions_dict as dim_exdict
-import coupanal.sample_dict as smpl_dict
 import MetBot.mytools as my
 import MetBot.mynetcdf as mync
 import MetBot.SynopticAnatomy as sy
 import MetBot.MetBlobs as blb
+import MetBot.EventStats as stats
+
 
 # Running options
-threshtest=True
+threshtest=False
 sub='SA'    # domain
 seas='NDJFM'
+from_event='all' # 'all' for all dates, 'first' for first in each event
+rm_samedates=False # to prune event set for matching dates - does not currently work for spatiofreq
 
 # part a - spatiofreq
 sfdom='SA_TR'
 rate='cbs' # if rate='year' it will plot cbs per year
             # if cbs it will plot for that models total number of CBs
+nos4cbar = (20, 50, 3)
 res='native'              # Option to plot at 'noaa' res or 'native' res
 
 # part b - outlines
 plotshow='col5' # 'greyall' or 'col5'
+messr=4 # added to first CB to change which 5 are selected
 
 # part c - centroids
+
 
 # part d - CV map
 whichdays='cbonly' # 'all' for all days in period
@@ -81,8 +86,6 @@ ncout = mync.open_multi(allfile, globv, name, \
 print '...file opened'
 alldata, time, lat, lon, dtime = ncout
 dtime[:, 3] = 0
-nlat = len(lat)
-nlon = len(lon)
 
 # Get threshold
 print 'getting threshold....'
@@ -112,29 +115,194 @@ for t in range(nthresh):
     print 'opening metbot files...'
     outsuf = botpath + name + '_'
     syfile = outsuf + thre_str + '_' + dset + '-OLR.synop'
-    # mbsfile = outsuf + thre_str + '_' + dset + "-olr-0-0.mbs"
-    # refmbs, refmbt, refch = blb.mbopen(mbsfile)
-    # refmbt[:, 3] = 0
     s = sy.SynopticEvents((), [syfile], COL=False)
     ks = s.events.keys();
     ks.sort()  # all
     refkey = s.mbskeys[0]
 
+    print 'get subset of TTT keys, dates, chs, centroids'
     edts = []
+    thesekeys = []
+    chs=[]
+    cXs=[]
+    cYs=[]
+    ecnt=0
     for k in ks:
         e = s.events[k]
-        dts = s.blobs[key]['mbt'][e.ixflags]
-        # Get just one date from each event
-        if len(dts) > 1:
-            dt = dts[len(dts) / 2]
-        else:
-            dt = dts[0]
-        # Restrict by season
-        if (int(dt[1]) >= f_mon) or (int(dt[1]) <= l_mon):
-            edts.append(dt)
+        dts = s.blobs[refkey]['mbt'][e.ixflags]
+        firstdate=dts[0]
+        # Restrict by season - using first day of event
+        if (int(firstdate[1]) >= f_mon) or (int(firstdate[1]) <= l_mon):
+            thesekeys.append(k)
+            if from_event=='first':
+                # If from first select the first date
+                if rm_samedates:
+                    #first check if already in list
+                    if ecnt==0:
+                        edts.append(firstdate)
+                        chs.append(e.blobs[refkey]['ch'][e.trk[0]])
+                        x, y = e.trkcX[0], e.trkcY[0]
+                        cXs.append(x)
+                        cYs.append(y)
+                    else:
+                        tmpedts=np.asarray(edts)
+                        ix = my.ixdtimes(tmpedts, [firstdate[0]], [firstdate[1]], [firstdate[2]], [firstdate[3]])
+                        if len(ix)==0:
+                            edts.append(firstdate)
+                            chs.append(e.blobs[refkey]['ch'][e.trk[0]])
+                            x, y = e.trkcX[0], e.trkcY[0]
+                            cXs.append(x)
+                            cYs.append(y)
+                else:
+                    edts.append(firstdate)
+                    chs.append(e.blobs[refkey]['ch'][e.trk[0]])
+                    x, y = e.trkcX[0], e.trkcY[0]
+                    cXs.append(x)
+                    cYs.append(y)
+            elif from_event=='all':
+                # if not from first loop all dates in event
+                for dt in range(len(dts)):
+                    thisdate=dts[dt]
+                    if rm_samedates:
+                        # first check if it is already in the list
+                        if ecnt == 0:
+                            edts.append(thisdate)
+                            chs.append(e.blobs[refkey]['ch'][e.trk[dt]])  # I think this selects first day
+                            x, y = e.trkcX[dt], e.trkcY[dt]
+                            cXs.append(x)
+                            cYs.append(y)
+                        else:
+                            tmpedts = np.asarray(edts)
+                            ix = my.ixdtimes(tmpedts, [thisdate[0]], [thisdate[1]], [thisdate[2]], [thisdate[3]])
+                            if len(ix) == 0:
+                                edts.append(thisdate)
+                                chs.append(e.blobs[refkey]['ch'][e.trk[dt]])  # I think this selects first day
+                                x, y = e.trkcX[dt], e.trkcY[dt]
+                                cXs.append(x)
+                                cYs.append(y)
+                    else:
+                        edts.append(thisdate)
+                        chs.append(e.blobs[refkey]['ch'][e.trk[dt]])  # I think this selects first day
+                        x, y = e.trkcX[dt], e.trkcY[dt]
+                        cXs.append(x)
+                        cYs.append(y)
+        ecnt+=1
     edts = np.asarray(edts)
+    # change hrtime to zero
+    edts[:, 3] = 0
     yrs = np.unique(edts[:, 0])
+    chs = np.asarray(chs)
+    cXs=np.asarray(cXs)
+    cYs=np.asarray(cYs)
+
+    # Open plot
+    plt.figure(figsize=[10, 8])
+
+    print 'Plotting part (a): spatiofreq'
+    plt.subplot(yplots,xplots,1)
+    m = blb.AfrBasemap2(lat, lon, drawstuff=True, prj='cyl',
+                         rsltn='l')
+    allmask = stats.spatiofreq5(m, s, name, lat, lon, yrs, thesekeys, per=rate, clim=nos4cbar, \
+                                savefig=False, flagonly=True, \
+                                col='bw', frm_event=from_event,cbar='none',title='')
+    m.drawcountries(color='k')
+    m.drawcoastlines(color='k')
+    plt.savefig('tmpfig_a.png', dpi=150)
+
+    print 'Plotting part (b): CB outlines'
+    plt.subplot(yplots,xplots,2)
+    m = blb.AfrBasemap2(lat, lon, drawstuff=True, prj='cyl',
+                         rsltn='l')
+    if plotshow == 'col5':
+        nch = 5
+        cols = ['r', 'b', 'c', 'm', 'g']
+
+    for jl in range(nch):
+        cb = chs[jl + messr]
+        if plotshow == 'greyall':
+            cl = 'darkgray'
+        else:
+            cl = cols[jl]
+        cnx, cny = cb[:, 0], cb[:, 1]
+        m.plot(cnx, cny, cl, lw=1.)
+
+    m.drawcountries()
+    m.drawcoastlines()
+    plt.savefig('tmpfig_b.png', dpi=150)
 
 
+    print 'Plotting part (c): lat / lon scatter'
+    plt.subplot(yplots,xplots,3)
+    m = blb.AfrBasemap2(lat, lon, drawstuff=True, prj='cyl',
+                         rsltn='l')
+    m.plot(cXs, cYs, c='k', marker="o", markersize=0.1)
+    # plt.scatter(cXs, cYs, c='k', marker="o", s=0.1, edgecolors='face')
+    # plt.xlim(7.5, 100.0)
+    # xlabs = [20, 40, 60, 80]
+    # plt.xticks(xlabs, fontsize=6)
+    # plt.ylim(-40.0, -15.0)
+    # ylabs = [-40, -30, -20]
+    # plt.yticks(ylabs, fontsize=6)
+    plt.savefig('tmpfig_c.png', dpi=150)
 
 
+    print 'Plotting part (d): CV OLR'
+    plt.subplot(yplots,xplots,4)
+    m = blb.AfrBasemap2(lat, lon, drawstuff=True, prj='cyl',
+                         rsltn='l')
+
+    if whichdays=='cbonly':
+
+        indices_m1=[]
+        for dt in range(len(edts)):
+            date=edts[dt]
+
+            ix = my.ixdtimes(dtime, [date[0]], [date[1]], [date[2]], [0])
+            if len(ix) >= 1:
+                indices_m1.append(ix)
+
+        indices_m1 = np.squeeze(np.asarray(indices_m1))
+
+        # Select these dates
+        olr_sel = alldata[indices_m1, :, :]
+        dtime_sel = dtime[indices_m1]
+
+    elif whichdays=='all':
+        olr_sel=alldata[:]
+        dtime_sel=dtime[:]
+
+    if whichstat=='std':
+        data4map=np.nanstd(olr_sel,0)
+    else:
+        cv=scipy.stats.variation(olr_sel,0,nan_policy='omit')
+        if whichstat=='cv':
+            data4map=cv[:]
+        elif whichstat=='cvper':
+            data4map=cv*100
+
+    plon, plat = np.meshgrid(lon, lat)
+    clevs = np.arange(0, 20, 2)
+    cm = plt.cm.RdPu
+    cs = m.contourf(plon, plat, data4plot, clevs, cmap=cm, extend='both')
+    m.drawcountries()
+    m.drawcoastlines()
+    plt.savefig('tmpfig_d.png', dpi=150)
+
+
+    # Finalising plot
+    plt.subplots_adjust(left=0.05, right=0.9, top=0.95, bottom=0.02, wspace=0.1, hspace=0.2)
+
+    # Add cbar for spatio freq
+    # f, ax = plt.gcf(), plt.gca()  # get reference and set axes
+    # axcol=f.addaxes([0.25,0.5,0.25,0.01])
+    # cm = plt.cm.gist_gray_r
+    # clim=nos4cbar[:]
+    # bounds=np.arange(clim[0],clim[1]+clim[2],clim[2])
+    # plt.colorbar(cax=axcol,bounds=bounds)
+    # plt.axes(ax)
+
+    figname = figdir + '/CBvar_4panelfig.' + seas + '.' + res + '.' + sub + '.per_' + rate + '.' + \
+              thnames[t] + '.png'
+    print 'saving figure as ' + figname
+    plt.savefig(figname, dpi=150)
+    plt.close()
