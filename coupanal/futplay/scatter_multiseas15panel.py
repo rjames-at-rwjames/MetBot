01523db22e9ef6385e0f77160ecb823d8bf8a55e
rjames-at-rwjames@github.com <rachel.james@eci.ox.ac.uk>
2019-08-19 11:55:04 +0200

    small changes to get more sesonal cycle figs

--- coupanal/futplay/scatter_multiseas15panel.py
+++ coupanal/futplay/scatter_multiseas15panel.py
@@ -57,13 +57,13 @@ weightlats=True
 
 # Info for change 1 (x axis)
 charac='relative' # options: 'number', 'relative', 'intens', 'tttpr'
-wlon=7.5
-elon=55.0
-# wlon=45.0
-# elon=70.0
+# wlon=7.5
+# elon=55.0
+wlon=45.0
+elon=70.0
 #ttt_dom=='subt'
-ttt_dom='contsub_nh' # domain for averaging TTT precip
-#ttt_dom='madasub_nh'
+# ttt_dom='contsub_nh' # domain for averaging TTT precip
+ttt_dom='madasub_nh'
 #ttt_dom='rufiji'
 #ttt_dom='malawi'
 under_of='day_of'
@@ -71,8 +71,8 @@ under_of='day_of'
 # Info for change 2 (y axis)
 globp='pr'
 #pr_dom='subt'
-pr_dom='contsub_nh'
-#pr_dom='madasub_nh'
+# pr_dom='contsub_nh'
+pr_dom='madasub_nh'
 #pr_dom='rufiji'
 #pr_dom='malawi'
 
